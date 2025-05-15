9be0eeccaa1fdb9ab88bf26ab828fae3814c764c
Daniel <daniel@Daniels-MBP.attlocal.net>
2025-05-15 14:24:05 -0400

    mongodb uri

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 try {
-    mongoose.connect(process.env.DB_URI).then((result) => {
+    mongoose.connect(process.env.NODE_ENV === 'developement' ? process.env.DB_URI : process.env.PROD_DB_URI).then((result) => {
         if (result) {
             console.log('moongoose connected')
         }
