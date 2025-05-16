5af3451541ee413ef7c3c9ed2551cb4fe113430a
Daniel <daniel@Daniels-MBP.attlocal.net>
2025-05-16 15:03:34 -0400

    deploy preview

--- backend/controllers/LogOff.js
+++ backend/controllers/LogOff.js
@@ -4,7 +4,7 @@ const LogOff = (req, res) => {
     res.clearCookie('token', {
         httpOnly: process.env.NODE_ENV !== 'development',
         secure: process.env.NODE_ENV !== 'development',
-        sameSite: process.env.NODE_ENV !== 'development' ? 'Strict' : 'Lax',
+        sameSite: process.env.NODE_ENV == 'production' ? "None" : 'Lax',
     });
 
     return res.status(200).json({ message: 'Logged out successfully' });
