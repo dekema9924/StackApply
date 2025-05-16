9c77e6d159dadd57cb076fbdeee969d5f455ff1f
Daniel <daniel@Daniels-MBP.attlocal.net>
2025-05-15 20:54:47 -0400

    prodURI

--- frontend/src/config/Config.tsx
+++ frontend/src/config/Config.tsx
@@ -6,5 +6,5 @@ export const Config = {
         : import.meta.env.VITE_PROD_API_URL
 };
 
-console.log(import.meta.env.DEV)
-console.log(import.meta.env.VITE_PROD_API_URL)
+console.log({ nodeenv: import.meta.env.DEV })
+console.log('USING API:', import.meta.env.DEV ? import.meta.env.VITE_DEV_API_URL : import.meta.env.VITE_PROD_API_URL);
