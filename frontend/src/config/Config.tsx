136c1ae1cb656af85b9e31b4b51365d45eddca9a
Daniel <daniel@Daniels-MBP.attlocal.net>
2025-05-15 21:43:15 -0400

    removed PRODURI

--- frontend/src/config/Config.tsx
+++ frontend/src/config/Config.tsx
@@ -3,13 +3,10 @@
 
 export const Config = {
     apiUrl: import.meta.env.DEV
-        ? import.meta.env.VITE_DEV_API_URL
-        : import.meta.env.VITE_PROD_API_URL
+        ? 'http://localhost:3000'
+        : 'https://stackapply.onrender.com'
 };
 
-console.log("MODE:", import.meta.env.MODE);
-console.log("DEV:", import.meta.env.DEV);
-console.log("PROD URL:", import.meta.env.VITE_PROD_API_URL);
-console.log("All env vars:", import.meta.env);
+console.log({ url: Config.apiUrl })
 
 
